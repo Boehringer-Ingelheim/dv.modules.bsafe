8188fdeb9dec19f3c21fa65e0e6c7bf11fa67595
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2026-04-07 13:03:36 +0200

    update test mock app

--- tests/testthat/test-bsafe_app.R
+++ tests/testthat/test-bsafe_app.R
@@ -10,7 +10,7 @@ local({
 # app tests ----
 local({
   test_that("tm_bsafe starts", {
-    app <- suppressWarnings(shinytest2::AppDriver$new(dv.modules.bsafe:::mock_app()))
+    app <- suppressWarnings(shinytest2::AppDriver$new(dv.modules.bsafe:::mock_bsafe()))
     app$wait_for_idle()
     checkmate::expect_string(app$get_html("body"), min.chars = 1)
   })
